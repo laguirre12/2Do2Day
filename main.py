35a2073be02d08e3d1f3f409a4b53decb4c315a3
gEvent <gevent@CSSI2015-6.local>
2015-07-27 11:19:32 -0400

    main.py is good

--- main.py
+++ main.py
@@ -25,6 +25,31 @@ class StressHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('templates/stress.html')
         self.response.write(template.render({"phrase":"eventually send in"}))
 
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+        self.response.write(template.render({"phrase":"eventually send in"}))
+
+class ExerciseHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/exercise.html')
+        self.response.write(template.render({"phrase":"eventually send in"}))
+
+class SchoolHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/school.html')
+        self.response.write(template.render({"phrase":"eventually send in"}))
+
+class CrazyHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/crazy.html')
+        self.response.write(template.render({"phrase":"eventually send in"}))
+
+class ChillHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/chill.html')
+        self.response.write(template.render({"phrase":"eventually send in"}))
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/work',WorkHandler),
