47aadf06df3de65a20e1629a9671c2166d7364a6
gEvent <gevent@CSSI2015-11.local>
2015-07-27 10:26:36 -0400

    created main.py file

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import webapp2
+import jinja2
+from google.appengine.ext import ndb
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
