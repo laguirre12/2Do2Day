f458947f12ea8ea84e0b2713068d119ec0c41fa6
gEvent <gevent@CSSI2015-6.local>
2015-07-29 09:38:44 -0400

    stuff

--- main.py
+++ main.py
@@ -20,7 +20,15 @@ class WorkHandler(webapp2.RequestHandler):
                  "During your break, walk around the office.",
                  "Organize your workspace.",
                  "Remember to sit or stand up straight and not slouch as much as you can.",
-                 "Try meditating/relaxing by sitting still for 2-5 minutes during your break.",]
+                 "Try meditating/relaxing by sitting still for 2-5 minutes during your break.",
+                 "Go have an interesting conversation with your boss that is not about work.",
+                 "Try to focus on your work by not using your phone/computer as much as you can.",
+                 "Try reflecting on your day after work (or doing work) and evaluate your accomplishments.",
+                 "Make sure to say Hello and Goodbye to your coworkers when entering and leaving the office. (bonus points for smiling while doing that)",
+                 "Go home! (After you finished your work :D )",
+                 "Create a to-do list with realistic goals and tasks for you to perform.",
+                 "Keep track of what you do during the day. Now how much time it took for you to do the work.",
+                 "Take some time to learn more about the industry that you are working in."]
         self.response.write(template.render({"phrase":actions[random.randint(0,len(actions)-1)]}))
 
 class MusicHandler(webapp2.RequestHandler):
@@ -54,18 +62,33 @@ class StressHandler(webapp2.RequestHandler):
                  'Go for a run.',
                  'Pet a dog and/or a cat!',
                  'Make yourself a nice cup of tea.',
-                 'Soak in a warm bath.']
+                 'Soak in a warm bath.',
+                 'Listen to music. (Have you tried or music section?)',
+                 'Go have a nice talk with a friend.',
+                 'Talk out your problems to yourself.',
+                 'Eat a nice healthy meal.',
+                 'Try to read some jokes and laugh away your stress',
+                 'Try keeping a stress journal where you write about your problems.',
+                 'Try taking a short nap.',
+                 'Try doing an arts and crafts project.']
         self.response.write(template.render({"phrase":actions[random.randint(0,len(actions)-1)]}))
 
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        actions=['Make a fort out of your cushions, pillows, and blankets!',
+        actions=['Make a fort out of your cushions, pillows, and blankets! (bonus points for getting friends to help you!!)',
                  'Take a nap!',
                  'Call a friend.',
+                 'Try texting a good friend a message backwards (ex: "elpmaxe") and see how long it takes for them to decode the message.',
                  'Bake something for yourself and/or friends.',
-                 'Plan a sleepover!']
+                 'Plan a sleepover!',
+                 'Speak only in rhymes!!',
+                 'Try to find Narnia!',
+                 'Have a costume Party w/ friends! (not just a Halloween activity)',
+                 'Read a book! (JK Just watch a movie instead)',
+                 'Watch cute cat videos on YouTube!!!!',
+                 'Bake Cookies! (props for sharing them with friends)']
         self.response.write(template.render({"phrase":actions[random.randint(0,len(actions)-1)]}))
 
 class ExerciseHandler(webapp2.RequestHandler):
@@ -75,7 +98,14 @@ class ExerciseHandler(webapp2.RequestHandler):
                  'Try making yourself a protein shake!',
                  'Try playing a sport with your friends regularly so you have more motivation for working out.',
                  'If you are used to exercising in a gym try exercising outside the gym for a change or vise versa.',
-                 'Try changing your exercise routine (try an early morning exercise routine or late night routine).']
+                 'Try changing your exercise routine (try an early morning exercise routine or late night routine).',
+                 'Make sure to do a lot of breathing!! (Seriously)',
+                 'Try listening to music while exercising. (check our music section)',
+                 'Try not listening to music while exercising in order to focus on your form.',
+                 'Try listening/watching to a podcast or a TV show while exercising.',
+                 'Try joining a workout class or an exercise group.',
+                 'Challenge yourself!!!',
+                 'Watch some workout motivation videos on YouTube before starting.']
         self.response.write(template.render({"phrase":actions[random.randint(0,len(actions)-1)]}))
 
 
@@ -97,7 +127,8 @@ class CrazyHandler(webapp2.RequestHandler):
                  'Take a hike.',
                  'Climb a mountain.',
                  'Swim with dolphins.',
-                 'Go bungee jumping!']
+                 'Go bungee jumping!',
+                 'Do something crazy!!']
         self.response.write(template.render({"phrase":actions[random.randint(0,len(actions)-1)]}))
 
 
