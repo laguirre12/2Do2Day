2fab257dc70422adaecf1675c451d0d445166194
gEvent <gevent@CSSI2015-6.local>
2015-07-30 11:17:11 -0400

    message

--- javascript/index_script.js
+++ javascript/index_script.js
@@ -14,26 +14,61 @@ function make_about_Appear()
   $('#home').addClass('hidden');
 };
 
-console.log("hello");
 
 $(document).ready(function(){
   pos = $("#work").position()
   $("#work").mouseover(function(){
-      console.log("hello2");
-      /*$("#index table").animate({bottom: '150px'});
-      $("#work").animate({left: '15px'});
-      $("#work").backgroundImage = "url('/images/exercise.png')";*/
-      $("#work").addClass("angled");
+      $("#work").addClass("left_angled");
   });
 });
 
 $(document).ready(function(){
   pos = $("#work").position()
   $("#work").mouseleave(function(){
-      console.log("hello3");
-      /*$("#index table").animate({bottom: '100px'});
-      $("#work").animate({left: '10px'});
-      $("#work").backgroundImage = "url('/images/suitcase.png')";*/
-      $("#work").removeClass("angled");
+      $("#work").removeClass("left_angled");
+  });
+});
+
+$(document).ready(function(){
+  pos = $("#school").position()
+  $("#school").mouseover(function(){
+      $("#school").addClass("right_angled");
+  });
+});
+
+$(document).ready(function(){
+  pos = $("#school").position()
+  $("#school").mouseleave(function(){
+      $("#school").removeClass("right_angled");
+  });
+});
+
+
+/* -------------------------------------------------------- */
+$(document).ready(function(){
+  pos = $("#exercise").position()
+  $("#exercise").mouseover(function(){
+      $("#exercise").addClass("left_angled");
+  });
+});
+
+$(document).ready(function(){
+  pos = $("#exercise").position()
+  $("#exercise").mouseleave(function(){
+      $("#exercise").removeClass("left_angled");
+  });
+});
+
+$(document).ready(function(){
+  pos = $("#stay_home").position()
+  $("#stay_home").mouseover(function(){
+      $("#stay_home").addClass("right_angled");
+  });
+});
+
+$(document).ready(function(){
+  pos = $("#stay_home").position()
+  $("#stay_home").mouseleave(function(){
+      $("#stay_home").removeClass("right_angled");
   });
 });
